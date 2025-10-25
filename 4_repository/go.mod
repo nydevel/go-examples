9453190f3813a74fd457b32be5844c7ae9d7016c
nydevel <1@shifu.io>
2025-10-25 23:28:46 +0500

    add repository example

---
+++ 4_repository/go.mod
@@ -0,0 +1,3 @@
+module 4_repository
+
+go 1.25
---
+++ 4_repository/main.go
@@ -0,0 +1,52 @@
+package main
+
+type User struct {
+	Name string
+	Age  int
+}
+
+type UserRepositoryInterface interface {
+	GetByName(name string) (*User, error)
+}
+
+type PGRepository struct {
+	users map[int]User
+}
+
+func PGRepoInit() *PGRepository {
+	return &PGRepository{
+		users: make(map[int]User),
+	}
+}
+
+func (r *PGRepository) GetByName(name string) (*User, error) {
+	user = User{
+		Name: "Pavel",
+		Age:  11,
+	}
+
+	return user, nil
+}
+
+type MSRepository struct {
+	users map[int]User
+}
+
+func MSRepoInit() *MSRepository {
+	return &MSRepository{
+		users: make(map[int]User),
+	}
+}
+
+func (r *MSRepository) GetByName(name string) (*User, error) {
+	user = User{
+		Name: "Pavel",
+		Age:  11,
+	}
+
+	return user, nil
+}
+
+func main() {
+	var _ UserRepositoryInterface = PGRepoInit()
+}
