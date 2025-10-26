a43008c9be98858a7fb6f0b0d06425281b041afd
nydevel <1@shifu.io>
2025-10-26 10:54:11 +0500

    add outer block shutdown

---
+++ 5_concurrency/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Server struct {
+	shutdown chan struct{} //empty struct channel takes 0 memory
+}
+
+func (s *Server) ListenAndServe() {
+free: //this will give are mark to break outer loop from switch on line 18
+	for {
+		select {
+		case <-s.shutdown:
+			fmt.Println("attemt to gracefull shutdown")
+			break free
+		default:
+		}
+	}
+}
+
+func (s *Server) quit() {
+	close(s.shutdown)
+}
+
+func main() {
+	srv := &Server{
+		shutdown: make(chan struct{}),
+	}
+
+	go func() {
+		time.Sleep(1 * time.Second)
+		srv.quit()
+	}()
+
+	srv.ListenAndServe()
+}
